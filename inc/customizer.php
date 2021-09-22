d7d7ac014d4dde000ee5f750f168584797a00c9e
Gian Pumayalla <gian.pumayalla@gmail.com>
2021-09-23 01:05:34 +0200

    first commit

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * wde-wp-theme Theme Customizer
+ *
+ * @package wde-wp-theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function wde_wp_theme_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'wde_wp_theme_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'wde_wp_theme_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'wde_wp_theme_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function wde_wp_theme_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function wde_wp_theme_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function wde_wp_theme_customize_preview_js() {
+	wp_enqueue_script( 'wde-wp-theme-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'wde_wp_theme_customize_preview_js' );
